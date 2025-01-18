cc6d25f77980f1e37816496dc36e51460c745bd4
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-01-18 13:06:19 +0330

    add swagger

--- Ecommerce/Services/Catalog/Catalog.API/Program.cs
+++ Ecommerce/Services/Catalog/Catalog.API/Program.cs
@@ -5,6 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Catalog.API", Version = "v1" , Description = "Catalog API"});
+});
 
 var app = builder.Build();
 
@@ -12,6 +17,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseAuthorization();
