348011173f199afa87bf30f7060f9a81a7674ad1
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-01-19 14:52:29 +0330

    Implementing Catalog Context

---
+++ Ecommerce/Services/Catalog/Catalog.Infrastructure/Data/CatalogContext.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.Core.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.Infrastructure.Data;
+
+public class CatalogContext : ICatalogContext
+{
+    public IMongoCollection<Product> Products { get; }
+    public IMongoCollection<ProductType> Types { get; }
+    public IMongoCollection<ProductBrand> Brands { get; }
+
+    public CatalogContext(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+        //
+        Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+        Brands = database.GetCollection<ProductBrand>(
+            configuration.GetValue<string>("DatabaseSettings:BrandsCollection"));
+        Types = database.GetCollection<ProductType>(configuration.GetValue<string>("DatabaseSettings:TypesCollection"));
+        //Seed Data
+        BrandSeedData.SeedData(Brands);
+        TypeSeedData.SeedData(Types);
+        ProductSeedData.SeedData(Products);
+    }
+}
