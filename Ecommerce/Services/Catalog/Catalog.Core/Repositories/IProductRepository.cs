f847406a83ef8dad1ca21f9505df6cea550ee349
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-01-19 12:38:16 +0330

    Create Repository for Catalog

---
+++ Ecommerce/Services/Catalog/Catalog.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.Core.Entities;
+
+namespace Catalog.Core.Repositories;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetProducts();
+    Task<Product> GetProduct(string id);
+    Task<IEnumerable<Product>> GetProductsByName(string name);
+    Task<IEnumerable<Product>> GetProductsByType(string type);
+    Task<IEnumerable<Product>> GetProductsByBrand(string brand);
+    Task<bool> UpdateProduct(Product product);
+    Task<bool> DeleteProduct(string id);
+}
---
+++ Ecommerce/Services/Catalog/Catalog.Core/Services/BrandRepository.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+
+namespace Catalog.Core.Services;
+
+public class BrandRepository : IBrandRepository
+{
+    public Task<IEnumerable<ProductBrand>> GetBrands()
+    {
+        throw new NotImplementedException();
+    }
+}
