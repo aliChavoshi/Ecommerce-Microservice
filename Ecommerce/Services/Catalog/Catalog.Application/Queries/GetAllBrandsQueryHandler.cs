d90e4f8618d200c561591c9404ff906e2f768acd
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-01-22 11:35:13 +0330

    Add brand querying and mapping functionality
    Updated `Catalog.Application.csproj` to remove `Responses` and `Queries` folders. Added `ProfileMapper` class for mapping `ProductBrand` to `BrandResponse` using AutoMapper. Introduced `GetAllBrandsQuery` and `GetAllBrandsQueryHandler` to fetch and map brand data. Created `BrandResponse` class with `Id` and `Name` properties.

---
+++ Ecommerce/Services/Catalog/Catalog.Application/Queries/GetAllBrandsQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Queries;
+
+public class GetAllBrandsQuery : IRequest<IEnumerable<BrandResponse>>
+{
+}
+
+public class GetAllBrandsQueryHandler(IBrandRepository brandRepository, IMapper mapper)
+    : IRequestHandler<GetAllBrandsQuery, IEnumerable<BrandResponse>>
+{
+    public async Task<IEnumerable<BrandResponse>> Handle(GetAllBrandsQuery request, CancellationToken cancellationToken)
+    {
+        var result = await brandRepository.GetBrands();
+        return mapper.Map<IEnumerable<BrandResponse>>(result);
+    }
+}
