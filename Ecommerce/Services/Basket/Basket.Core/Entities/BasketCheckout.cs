db67a3b3c8930036ac137549989017c891b034c8
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-01-26 12:17:26 +0330

    add new entities for basket

---
+++ Ecommerce/Services/Basket/Basket.Core/Entities/BasketCheckout.cs
@@ -0,0 +1,27 @@
+﻿namespace Basket.Core.Entities;
+
+public class BasketCheckout
+{
+    public string UserName { get; set; }
+    public decimal TotalPrice { get; set; }
+
+    //
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress { get; set; }
+    public string BuyerPhoneNumber { get; set; }
+    public string AddressLine { get; set; }
+    public string State { get; set; }
+    public string City { get; set; }
+
+    //
+    public int PaymentMethod { get; set; }
+    public string Status { get; set; }
+
+    public string TrackingCode { get; set; }
+    //
+    // public string CardName { get; set; }
+    // public string CardNumber { get; set; }
+
+    //
+}
---
+++ Ecommerce/Services/Basket/Basket.Core/Entities/ShoppingCart.cs
@@ -0,0 +1,12 @@
+﻿namespace Basket.Core.Entities;
+
+public class ShoppingCart(string userName)
+{
+    public string UserName { get; set; } = userName;
+    public List<ShoppingCartItem> Items { get; set; } = new();
+
+    public decimal CalculateOriginalPrice()
+    {
+        return Items.Sum(x => x.Price * x.Quantity);
+    }
+}
