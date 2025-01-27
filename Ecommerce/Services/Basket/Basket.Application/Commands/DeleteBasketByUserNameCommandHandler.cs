c74e93218d506c95bad85bf7bc498f4373ba5a90
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-01-27 14:28:05 +0330

    Add API versioning and delete basket command handler
    Updated Basket.API.csproj to include API versioning packages.
    Added new ApiController with versioning attributes and routing.
    Implemented DeleteBasketByUserNameCommand and handler.

---
+++ Ecommerce/Services/Basket/Basket.Application/Commands/DeleteBasketByUserNameCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using Basket.Core.Repositories;
+using MediatR;
+
+namespace Basket.Application.Commands;
+
+public class DeleteBasketByUserNameCommand : IRequest<bool>
+{
+    public string UserName { get; set; }
+
+    public DeleteBasketByUserNameCommand(string userName)
+    {
+        UserName = userName;
+    }
+}
+public class DeleteBasketByUserNameCommandHandler(IBasketRepository basketRepository) : IRequestHandler<DeleteBasketByUserNameCommand,bool>
+{
+    public async Task<bool> Handle(DeleteBasketByUserNameCommand request, CancellationToken cancellationToken)
+    {
+        await basketRepository.DeleteBasket(request.UserName);
+        return true;
+    }
+}
