bdc943f99561c7de6b989bb0b10d6926e28190e4
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-01-27 14:46:43 +0330

    Add Swagger, API versioning, and Redis caching
    Updated Basket.API.csproj to include Swashbuckle.AspNetCore and reordered package references. Modified Dockerfile to copy additional project files. Enhanced Program.cs with new using statements, API versioning, Swagger support, AutoMapper, MediatR, Redis caching, and dependency injection for IBasketRepository. Updated launchSettings.json to change applicationUrl and enable browser launch with Swagger. Revised appsettings.json to remove logging and allowed hosts settings, and added Redis connection string.

--- Ecommerce/Services/Basket/Basket.API/Program.cs
+++ Ecommerce/Services/Basket/Basket.API/Program.cs
@@ -1,17 +1,54 @@
+using System.Reflection;
+using Asp.Versioning;
+using Basket.Application.Commands;
+using Basket.Core.Repositories;
+using Basket.Infrastructure.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Add API Versioning
+builder.Services.AddApiVersioning(options =>
+{
+    options.ReportApiVersions = true;
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Basket.API", Version = "v1", Description = "Basdket API" });
+});
+// Register Mapper
+builder.Services.AddAutoMapper(typeof(Program));
+//Register Mediatr
+var assemblies = new[]
+{
+    Assembly.GetExecutingAssembly(),
+    typeof(CreateShoppingCartCommandHandler).Assembly
+};
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assemblies));
+//Redis
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+//DI
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseAuthorization();
