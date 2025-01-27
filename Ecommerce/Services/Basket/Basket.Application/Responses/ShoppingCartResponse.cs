fba53ab33ecbfb11fcd508056d7ea408b76978ec
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-01-27 10:55:37 +0330

    Add new mappings, responses, and package references
    Updated Basket.Application.csproj to include new folders for Commands, Handlers, Responses, and Queries. Added AutoMapper (v13.0.1) and MediatR (v12.4.1) package references. Created BasketMappingProfile for mapping ShoppingCartItem to ShoppingCartItemResponse and ShoppingCart to ShoppingCartResponse. Added ShoppingCartItemResponse and ShoppingCartResponse classes with necessary properties and methods.

---
+++ Ecommerce/Services/Basket/Basket.Application/Responses/ShoppingCartResponse.cs
@@ -0,0 +1,23 @@
+﻿using Basket.Core.Entities;
+
+namespace Basket.Application.Responses;
+
+public class ShoppingCartResponse
+{
+    public string UserName { get; set; }
+    public List<ShoppingCartItemResponse> Items { get; set; } = new();
+
+    public ShoppingCartResponse()
+    {
+    }
+
+    public ShoppingCartResponse(string userName)
+    {
+        UserName = userName;
+    }
+
+    public decimal CalculateOriginalPrice()
+    {
+        return Items.Sum(x => x.Price * x.Quantity);
+    }
+}
