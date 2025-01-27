3591f76a5d674ce5cef63b27a676a2e7f56925ee
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-01-27 12:24:46 +0330

    Add GetBasketByUserName query and handler
    Introduced a new query and handler in GetBasketByUserNameQueryHandler.cs:
    - Added necessary using directives for AutoMapper, Basket.Application.Responses, Basket.Core.Repositories, and MediatR.
    - Defined GetBasketByUserNameQuery class implementing IRequest<ShoppingCartResponse> with a userName parameter.
    - Implemented GetBasketByUserNameQueryHandler class implementing IRequestHandler<GetBasketByUserNameQuery, ShoppingCartResponse>.
    - Handle method retrieves the basket for the given userName and maps it to ShoppingCartResponse using AutoMapper, or returns a new ShoppingCartResponse if no basket is found.

---
+++ Ecommerce/Services/Basket/Basket.Application/Queries/GetBasketByUserNameQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Basket.Application.Responses;
+using Basket.Core.Repositories;
+using MediatR;
+
+namespace Basket.Application.Queries;
+
+public class GetBasketByUserNameQuery(string userName) : IRequest<ShoppingCartResponse>
+{
+    public string UserName { get; set; } = userName;
+}
+
+public class GetBasketByUserNameQueryHandler(IBasketRepository basketRepository, IMapper mapper)
+    : IRequestHandler<GetBasketByUserNameQuery, ShoppingCartResponse>
+{
+    public async Task<ShoppingCartResponse> Handle(GetBasketByUserNameQuery request,
+        CancellationToken cancellationToken)
+    {
+        var basket = await basketRepository.GetBasket(request.UserName);
+        if (basket != null)
+        {
+            return mapper.Map<ShoppingCartResponse>(basket);
+        }
+
+        return new ShoppingCartResponse(request.UserName);
+    }
+}
