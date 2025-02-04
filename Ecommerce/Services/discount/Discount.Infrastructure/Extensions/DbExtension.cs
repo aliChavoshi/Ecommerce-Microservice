5b7cf9b9df5c91cc76f6dd1b2810a57aa617032d
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-02-04 12:32:03 +0330

    Add DbExtension class and implement DiscountRepository
    Added necessary using directives in DbExtension.cs. Introduced a new static class `DbExtension` in the `Discount.Infrastructure.Extensions` namespace with methods for database migration and initial data population. Modified `DiscountRepository` to include methods for CRUD operations on the `Coupon` table and removed `NotImplementedException` from all methods.

---
+++ Ecommerce/Services/discount/Discount.Infrastructure/Extensions/DbExtension.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+
+namespace Discount.Infrastructure.Extensions;
+
+public static class DbExtension
+{
+    public static IHost MigrateDatabase<TContext>(this IHost host)
+    {
+        using var scope = host.Services.CreateScope();
+        var services = scope.ServiceProvider;
+        var logger = services.GetRequiredService<ILogger<TContext>>();
+        var configuration = services.GetRequiredService<IConfiguration>();
+        try
+        {
+            logger.LogInformation("Migrating database associated with context {DbContextName}",
+                typeof(TContext).Name);
+            ApplyMigration(configuration);
+            logger.LogInformation("Migrated database associated with context {DbContextName}",
+                typeof(TContext).Name);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occurred while migrating the database used on context {DbContextName}",
+                typeof(TContext).Name);
+        }
+
+        return host;
+    }
+
+    private static void ApplyMigration(IConfiguration config)
+    {
+        var retry = 5;
+        while (retry > 0)
+        {
+            try
+            {
+                using var connection =
+                    new NpgsqlConnection(config.GetValue<string>("DatabaseSettings:ConnectionString"));
+                connection.Open();
+                using var cmd = new NpgsqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = "DROP TABLE IF EXISTS Coupon";
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY, 
+                                                    ProductName VARCHAR(500) NOT NULL,
+                                                    Description TEXT,
+                                                    Amount INT)";
+                cmd.ExecuteNonQuery();
+
+                cmd.CommandText =
+                    "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Adidas Quick Force Indoor Badminton Shoes', 'Shoe Discount', 500);";
+                cmd.ExecuteNonQuery();
+
+                cmd.CommandText =
+                    "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Yonex VCORE Pro 100 A Tennis Racquet (270gm, Strung)', 'Racquet Discount', 700);";
+                cmd.ExecuteNonQuery();
+                // Exit loop if successful
+                break;
+            }
+            catch (Exception ex)
+            {
+                retry--;
+                if (retry == 0)
+                {
+                    throw;
+                }
+
+                //Wait for 2 seconds
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
--- Ecommerce/Services/discount/Discount.Infrastructure/Services/DiscountRepository.cs
+++ Ecommerce/Services/discount/Discount.Infrastructure/Services/DiscountRepository.cs
@@ -1,27 +1,51 @@
-﻿using Discount.Core.Entities;
+﻿using Dapper;
+using Discount.Core.Entities;
 using Discount.Core.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
 
 namespace Discount.Infrastructure.Services;
 
-public class DiscountRepository : IDiscountRepository
+public class DiscountRepository(IConfiguration configuration) : IDiscountRepository
 {
-    public Task<Coupon> GetDiscount(string productName)
+    private readonly string? _connectionString = configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+
+    public async Task<Coupon> GetDiscount(string productName)
     {
-        throw new NotImplementedException();
+        await using var connection = new NpgsqlConnection(_connectionString);
+        var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>(
+            "SELECT * FROM Coupon WHERE ProductName = @ProductName", new { ProductName = productName });
+        return coupon ?? new Coupon
+            { Amount = 0, Description = "no discount available", ProductName = "No Discount" };
     }
 
-    public Task<bool> CreateDiscount(Coupon coupon)
+    public async Task<bool> CreateDiscount(Coupon coupon)
     {
-        throw new NotImplementedException();
+        await using var connection = new NpgsqlConnection(_connectionString);
+        var affected = await connection.ExecuteAsync(
+            "INSERT INTO Coupon (ProductName, Description, Amount) VALUES (@ProductName, @Description, @Amount)",
+            new { coupon.ProductName, coupon.Description, coupon.Amount });
+
+        return affected > 0;
     }
 
-    public Task<bool> UpdateDiscount(Coupon coupon)
+    public async Task<bool> UpdateDiscount(Coupon coupon)
     {
-        throw new NotImplementedException();
+        await using var connection = new NpgsqlConnection(_connectionString);
+        var affected = await connection.ExecuteAsync(
+            "UPDATE Coupon SET ProductName=@ProductName, Description=@Description, Amount=@Amount WHERE Id=@Id",
+            new { coupon.ProductName, coupon.Description, coupon.Amount, coupon.Id });
+
+        return affected > 0;
     }
 
-    public Task<bool> DeleteDiscount(string productName)
+    public async Task<bool> DeleteDiscount(string productName)
     {
-        throw new NotImplementedException();
+        await using var connection = new NpgsqlConnection(_connectionString);
+        var affected = await connection.ExecuteAsync(
+            "DELETE FROM Coupon WHERE ProductName = @ProductName",
+            new { ProductName = productName });
+
+        return affected > 0;
     }
 }
