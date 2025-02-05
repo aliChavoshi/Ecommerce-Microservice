e63e21e4a26faca042c540102d1f849f83ee13f5
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-02-05 14:34:14 +0330

    Update project to .NET 9, refactor Program.cs for gRPC
    Updated Discount.API.csproj:
    - Changed target framework to net9.0.
    - Removed Microsoft.AspNetCore.OpenApi package reference.
    - Added project reference to Discount.Application.
    
    Refactored Program.cs:
    - Added using directives for reflection, services, commands, mapping, interfaces, and extensions.
    - Moved WebApplication.CreateBuilder(args) and added service registrations:
      - Registered AutoMapper with DiscountProfile.
      - Registered MediatR with relevant assemblies.
      - Set up DI for IDiscountRepository with DiscountRepository.
      - Added gRPC services.
    - Updated HTTP request pipeline:
      - Used app.UseDeveloperExceptionPage() in development.
      - Replaced app.UseAuthorization() and app.MapControllers() with app.UseRouting(), app.MapGrpcService<DiscountService>(), and a default route for gRPC endpoints.
    - Kept app.Run() at the end of the pipeline configuration.

--- Ecommerce/Services/discount/Discount.API/Program.cs
+++ Ecommerce/Services/discount/Discount.API/Program.cs
@@ -1,21 +1,43 @@
+using System.Reflection;
+using Discount.API.Services;
+using Discount.Application.Commands;
+using Discount.Application.Mapper;
+using Discount.Core.Interfaces;
+using Discount.Infrastructure.Extensions;
+using Discount.Infrastructure.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
+// Register Mapper
+builder.Services.AddAutoMapper(typeof(DiscountProfile));
+//Register Mediatr
+var assemblies = new[]
+{
+    Assembly.GetExecutingAssembly(),
+    typeof(CreateDiscountCommandHandler).Assembly
+};
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assemblies));
+//DI
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+//GRPC
+builder.Services.AddGrpc();
+// 
 var app = builder.Build();
-
+//Migration
+app.MigrateDatabase<Program>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseDeveloperExceptionPage();
 }
 
-app.UseAuthorization();
-
-app.MapControllers();
+app.UseRouting();
+app.MapGrpcService<DiscountService>();
+app.MapGet("/",
+    async context =>
+    {
+        await context.Response.WriteAsync(
+            "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+    });
 
 app.Run();
