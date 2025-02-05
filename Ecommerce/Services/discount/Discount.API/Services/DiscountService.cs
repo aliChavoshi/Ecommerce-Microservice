36b7060d050087d1baaa765826b6337e14d03891
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-02-05 14:29:07 +0330

    Add DiscountService and update proto service definition
    - Added DiscountService class in DiscountService.cs with methods for GetDiscount, UpdateDiscount, DeleteDiscount, and CreateDiscount using IMediator.
    - Updated using directives in DiscountService.cs to include necessary namespaces.
    - Removed unnecessary <ItemGroup> from Discount.Application.csproj.
    - Renamed service definition in discount.proto from DiscountService to DiscountProtoService.

---
+++ Ecommerce/Services/discount/Discount.API/Services/DiscountService.cs
@@ -0,0 +1,41 @@
+﻿using Discount.Application.Commands;
+using Discount.Application.Protos;
+using Discount.Application.Queries;
+using Grpc.Core;
+using MediatR;
+
+namespace Discount.API.Services;
+
+public class DiscountService(IMediator mediator) : DiscountProtoService.DiscountProtoServiceBase
+{
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        var query = new GetDiscountQuery(request.ProductName);
+        var result = await mediator.Send(query);
+        return result;
+    }
+
+    public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        var command = new UpdateDiscountCommand(request.Coupon);
+        var result = await mediator.Send(command);
+        return result;
+    }
+
+    public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request,
+        ServerCallContext context)
+    {
+        var command = new DeleteDiscountCommand(request.ProductName);
+        var result = await mediator.Send(command);
+        return new DeleteDiscountResponse()
+        {
+            Success = result
+        };
+    }
+
+    public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        var command = new CreateDiscountCommand(request.Coupon);
+        return await mediator.Send(command);
+    }
+}
