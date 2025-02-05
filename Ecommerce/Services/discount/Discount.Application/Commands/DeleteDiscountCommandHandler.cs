eda85990e7ff825486088c72ad2c691a259ef827
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-02-05 12:51:16 +0330

    Add command and handler for deleting discounts
    Introduce `DeleteDiscountCommand` and `DeleteDiscountCommandHandler` to handle discount deletion by product name. The command encapsulates the product name, and the handler interacts with `IDiscountRepository` to perform the deletion. Added necessary `using` directives for `Discount.Core.Interfaces` and `MediatR`.

---
+++ Ecommerce/Services/discount/Discount.Application/Commands/DeleteDiscountCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using Discount.Core.Interfaces;
+using MediatR;
+
+namespace Discount.Application.Commands;
+
+public class DeleteDiscountCommand(string productName) : IRequest<bool>
+{
+    public string ProductName { get; set; } = productName;
+}
+
+public class DeleteDiscountCommandHandler(IDiscountRepository discountRepository)
+    : IRequestHandler<DeleteDiscountCommand, bool>
+{
+    public async Task<bool> Handle(DeleteDiscountCommand request, CancellationToken cancellationToken)
+    {
+        return await discountRepository.DeleteDiscount(request.ProductName);
+    }
+}
