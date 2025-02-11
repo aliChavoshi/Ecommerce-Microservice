a62e5b273e0e6c821d1616cafa58caea454dc025
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-02-11 14:15:54 +0330

    Add OrderContext with audit property handling
    Introduced a new OrderContext class in the Ordering.Infrastructure.Data namespace, inheriting from DbContext and configured with DbContextOptions<OrderContext>. This class includes a DbSet<Order> property for managing Order entities.
    
    Overrode SaveChangesAsync to automatically set audit properties (LastModifiedDate, LastModifiedBy, CreatedDate, CreatedBy) for entities derived from EntityBase. These properties are updated with the current date and a placeholder user ("Ali Chavoshi") during save operations.
    
    Added a comment suggesting the use of interceptors for setting audit properties.

---
+++ Ecommerce/Services/Ordering/Ordering.Infrastructure/Data/OrderContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Common;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data;
+
+public class OrderContext(DbContextOptions<OrderContext> options) : DbContext(options)
+{
+    public DbSet<Order> Orders => Set<Order>();
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
+    {
+        //we can use interceptors instead of this code
+        foreach (var entry in ChangeTracker.Entries<EntityBase>())
+        {
+            switch (entry.State)
+            {
+                case EntityState.Modified:
+                    entry.Entity.LastModifiedDate = DateTime.Now;
+                    entry.Entity.LastModifiedBy = "Ali Chavoshi"; //TODO
+                    break;
+                case EntityState.Added:
+                    entry.Entity.LastModifiedDate = DateTime.Now;
+                    entry.Entity.LastModifiedBy = "Ali Chavoshi"; //TODO
+                    entry.Entity.CreatedDate = DateTime.Now;
+                    entry.Entity.CreatedBy = "Ali Chavoshi"; //TODO
+                    break;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+};
