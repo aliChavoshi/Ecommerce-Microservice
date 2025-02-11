5c48c43f7e8c35b5b7f282b2f6f856b96c070e5d
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-02-11 14:21:24 +0330

    Add OrderContextFactory for EF Core design-time support
    Introduced OrderContextFactory in Ordering.Infrastructure.Data:
    - Added using directives for EF Core in OrderContextFactory.cs
    - Created OrderContextFactory class implementing IDesignTimeDbContextFactory<OrderContext>
    - Implemented CreateDbContext method to configure SQL Server with a placeholder connection string
    - Removed <Folder Include="Data\" /> from Ordering.Infrastructure.csproj for project structure cleanup

---
+++ Ecommerce/Services/Ordering/Ordering.Infrastructure/Data/OrderContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Ordering.Infrastructure.Data;
+
+public class OrderContextFactory : IDesignTimeDbContextFactory<OrderContext>
+{
+    public OrderContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<OrderContext>();
+        optionsBuilder.UseSqlServer("Data Source=OrderDb"); //TODO
+        return new OrderContext(optionsBuilder.Options);
+    }
+}
