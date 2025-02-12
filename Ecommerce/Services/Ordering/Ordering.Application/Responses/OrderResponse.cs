fdad00955dc6506b417402b0a8e558e6380f5a6c
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-02-12 12:47:02 +0330

    Add new packages and classes for Ordering service
    Updated Ordering.Application.csproj to include AutoMapper, FluentValidation, and MediatR packages. Added new folders for Commands, Mappers, Extensions, Responses, and Queries. Introduced OrderResponse class in Ordering.Application.Responses namespace. Added InfrastructureServices class in Ordering.Infrastructure namespace with AddInfrastructureServices method for IServiceCollection.

---
+++ Ecommerce/Services/Ordering/Ordering.Application/Responses/OrderResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace Ordering.Application.Responses;
+
+public class OrderResponse
+{
+    public int Id { get; set; }
+    public string? UserName { get; set; }
+    public decimal? TotalPrice { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? EmailAddress { get; set; }
+    public string? AddressLine { get; set; }
+    public string? Country { get; set; }
+    public string? State { get; set; }
+    public int? PaymentMethod { get; set; }
+}
---
+++ Ecommerce/Services/Ordering/Ordering.Infrastructure/InfrastructureServices.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Core.Repositories;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Repositories;
+
+namespace Ordering.Infrastructure;
+
+public static class InfrastructureServices
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<OrderContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString"));//TODO
+        });
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        services.AddScoped<IOrderRepository, OrderRepository>();
+        return services;
+    }
+}
