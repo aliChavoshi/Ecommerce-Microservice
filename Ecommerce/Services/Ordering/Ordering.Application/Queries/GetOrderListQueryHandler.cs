dd58477f0f1483b3efb4a5344a8ebc795f9912b0
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-02-15 12:19:02 +0330

    Add Order mapping profile and GetOrderList query handler
    Created OrderMappingProfile.cs for AutoMapper configuration.
    Updated Ordering.Application.csproj to remove Mappers and Queries folders.
    Added GetOrderListQuery and GetOrderListQueryHandler in GetOrderListQueryHandler.cs.
    Included necessary using directives for AutoMapper, MediatR, and other dependencies.

---
+++ Ecommerce/Services/Ordering/Ordering.Application/Queries/GetOrderListQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Ordering.Application.Responses;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+
+namespace Ordering.Application.Queries;
+
+public class GetOrderListQuery(string userName) : IRequest<List<OrderResponse>>
+{
+    public string UserName { get; set; } = userName;
+}
+
+public class GetOrderListQueryHandler(IOrderRepository orderRepository,IMapper mapper)
+    : IRequestHandler<GetOrderListQuery, List<OrderResponse>>
+{
+    public async Task<List<OrderResponse>> Handle(GetOrderListQuery request, CancellationToken cancellationToken)
+    {
+        var result = await orderRepository.GetOrdersByUserName(request.UserName);
+        return mapper.Map<List<OrderResponse>>(result);
+    }
+}
