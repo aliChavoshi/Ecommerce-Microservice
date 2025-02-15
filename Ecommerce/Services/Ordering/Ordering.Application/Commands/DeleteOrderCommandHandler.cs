2d5d1e91279bdee4c6890503cdc425b21172cb9d
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-02-15 12:38:32 +0330

    Add DeleteOrderCommand and its handler
    Added necessary `using` directives for `MediatR`, `Ordering.Core.Entities`, and `Ordering.Core.Repositories`.
    Introduced a new namespace `Ordering.Application.Commands`.
    Created `DeleteOrderCommand` class implementing `IRequest` with an `Id` property.
    Implemented `DeleteOrderCommandHandler` class handling `DeleteOrderCommand`.
    In `DeleteOrderCommandHandler`, added async `Handle` method to retrieve and delete order by ID using `orderRepository`, throwing `OrderNotFoundException` if not found.

---
+++ Ecommerce/Services/Ordering/Ordering.Application/Commands/DeleteOrderCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+
+namespace Ordering.Application.Commands;
+
+public class DeleteOrderCommand : IRequest
+{
+    public int Id { get; set; }
+}
+public class DeleteOrderCommandHandler(IOrderRepository orderRepository) : IRequestHandler<DeleteOrderCommand>
+{
+    public async  Task Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+    {
+        var order = await orderRepository.GetByIdAsync(request.Id);
+        if(order==null) throw new OrderNotFoundException(nameof(Order), request.Id);
+        await orderRepository.DeleteAsync(order);
+    }
+}
