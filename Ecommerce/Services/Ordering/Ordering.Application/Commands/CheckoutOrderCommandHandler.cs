256a45543ac2224cb2dc052d35b4f31edaacc14b
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-02-15 12:28:10 +0330

    Add CheckoutOrderCommand and handler for order processing
    Introduced `CheckoutOrderCommand` and `CheckoutOrderCommandHandler`
    to handle checkout orders. Added necessary using directives in
    `CheckoutOrderCommandHandler.cs`. Updated `OrderMappingProfile`
    to include mapping between `CheckoutOrderCommand` and `Order`.

---
+++ Ecommerce/Services/Ordering/Ordering.Application/Commands/CheckoutOrderCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Responses;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+
+namespace Ordering.Application.Commands;
+
+public class CheckoutOrderCommand : IRequest<int>
+{
+    public string? UserName { get; set; }
+    public decimal? TotalPrice { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? EmailAddress { get; set; }
+    public string? AddressLine { get; set; }
+    public string? State { get; set; }
+    public int? PaymentMethod { get; set; }
+}
+
+public class CheckoutOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper)
+    : IRequestHandler<CheckoutOrderCommand, int>
+{
+    public async Task<int> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
+    {
+        var order = mapper.Map<Order>(request);
+        var result = await orderRepository.AddAsync(order);
+        return result.Id;
+    }
+}
