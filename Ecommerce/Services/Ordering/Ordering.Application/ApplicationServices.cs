53f625b459b667e1fac7e4689eadfb41cfa913af
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-02-15 14:06:52 +0330

    Add ApplicationServices class and update dependencies
    Introduce ApplicationServices class with AddApplicationServices
    extension method for IServiceCollection to register application
    services. Register AutoMapper, MediatR, FluentValidation, and
    pipeline behaviors. Update Ordering.Application.csproj to include
    FluentValidation.DependencyInjectionExtensions package.

---
+++ Ecommerce/Services/Ordering/Ordering.Application/ApplicationServices.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviour;
+using Ordering.Application.Mappers;
+
+namespace Ordering.Application;
+
+public static class ApplicationServices
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddAutoMapper(typeof(OrderMappingProfile));
+        services.AddMediatR(c => c.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationsBehaviour<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnHandledExceptionBehaviour<,>));
+        return services;
+    }
+}
