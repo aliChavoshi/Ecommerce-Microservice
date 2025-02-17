6b5425bae95c0c794217985a98c5fafee3ba4abd
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-02-17 12:57:28 +0330

    Add new event classes and update project settings
    - Introduce `EventBus.Messages.Common` namespace
    - Add `EventBusConstant` class with `BasketCheckoutQueue` constant
    - Modify `EventBus.Messages.csproj`:
      - Change `Nullable` property from `enable` to `disable`
      - Include `Events` and `Common` folders
    - Add `BaseIntegrationEvent` class with `CorrelationId` and `CreationDate` properties
    - Add `BasketCheckoutEvent` class inheriting from `BaseIntegrationEvent` with additional properties

---
+++ Ecommerce/EventBus.Messages/Events/BaseIntegrationEvent.cs
@@ -0,0 +1,19 @@
+﻿namespace EventBus.Messages.Common;
+
+public class BaseIntegrationEvent
+{
+    public string CorrelationId { get; set; }
+    public DateTime CreationDate { get; set; }
+
+    public BaseIntegrationEvent()
+    {
+        CorrelationId = Guid.NewGuid().ToString();
+        CreationDate = DateTime.Now;
+    }
+
+    public BaseIntegrationEvent(Guid correlationId, DateTime creationDate)
+    {
+        CorrelationId = correlationId.ToString();
+        CreationDate = creationDate;
+    }
+}
---
+++ Ecommerce/EventBus.Messages/Events/BasketCheckoutEvent.cs
@@ -0,0 +1,22 @@
+﻿namespace EventBus.Messages.Common;
+
+public class BasketCheckoutEvent : BaseIntegrationEvent
+{
+    public string UserName { get; set; }
+    public decimal TotalPrice { get; set; }
+
+    //
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress { get; set; }
+    public string BuyerPhoneNumber { get; set; }
+    public string AddressLine { get; set; }
+    public string State { get; set; }
+    public string City { get; set; }
+
+    //
+    public int PaymentMethod { get; set; }
+    public string Status { get; set; }
+
+    public string TrackingCode { get; set; }
+}
