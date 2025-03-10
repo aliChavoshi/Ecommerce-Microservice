e87f76f3095b4f5b4d23368f6746a9f8fb778cc9
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-03-10 11:02:46 +0330

    Update logging and refactor user seeding logic
    Modified Serilog configuration in Program.cs for consistent logging format. Refactored EnsureSeedData method in SeedData.cs to use modern syntax and updated user creation logic to use "alichavoshi" instead of "alice". Commented out unused code for user "bob" and added debug logging for user creation status.

--- Ecommerce/Infrastructure/IdentityServerAspNetIdentity/SeedData.cs
+++ Ecommerce/Infrastructure/IdentityServerAspNetIdentity/SeedData.cs
@@ -12,76 +12,79 @@ public class SeedData
 {
     public static void EnsureSeedData(WebApplication app)
     {
-        using (var scope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+        using var scope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+
+        var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+        var user = userMgr.FindByNameAsync("alichavoshi").Result;
+        if (user == null)
         {
-            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-            context.Database.Migrate();
-
-            var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-            var alice = userMgr.FindByNameAsync("alice").Result;
-            if (alice == null)
+            user = new ApplicationUser
             {
-                alice = new ApplicationUser
-                {
-                    UserName = "alice",
-                    Email = "AliceSmith@email.com",
-                    EmailConfirmed = true,
-                };
-                var result = userMgr.CreateAsync(alice, "Pass123$").Result;
-                if (!result.Succeeded)
-                {
-                    throw new Exception(result.Errors.First().Description);
-                }
-
-                result = userMgr.AddClaimsAsync(alice, new Claim[]{
-                            new Claim(JwtClaimTypes.Name, "Alice Smith"),
-                            new Claim(JwtClaimTypes.GivenName, "Alice"),
-                            new Claim(JwtClaimTypes.FamilyName, "Smith"),
-                            new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
-                        }).Result;
-                if (!result.Succeeded)
-                {
-                    throw new Exception(result.Errors.First().Description);
-                }
-                Log.Debug("alice created");
-            }
-            else
+                UserName = "alichavoshi",
+                Email = "alichavoshi@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+                PhoneNumber = "09130242780"
+            };
+            var result = userMgr.CreateAsync(user, "Pass123$").Result;
+            if (!result.Succeeded)
             {
-                Log.Debug("alice already exists");
+                throw new Exception(result.Errors.First().Description);
             }
 
-            var bob = userMgr.FindByNameAsync("bob").Result;
-            if (bob == null)
+            result = userMgr.AddClaimsAsync(user, [
+                new Claim(JwtClaimTypes.Name, "Ali Chavoshi"),
+                new Claim(JwtClaimTypes.GivenName, "alichavoshi"),
+                new Claim(JwtClaimTypes.FamilyName, "Chavoshi"),
+                new Claim(JwtClaimTypes.WebSite, "https://www.daneshjooyar.com/teacher/alichavoshi/")
+            ]).Result;
+            if (!result.Succeeded)
             {
-                bob = new ApplicationUser
-                {
-                    UserName = "bob",
-                    Email = "BobSmith@email.com",
-                    EmailConfirmed = true
-                };
-                var result = userMgr.CreateAsync(bob, "Pass123$").Result;
-                if (!result.Succeeded)
-                {
-                    throw new Exception(result.Errors.First().Description);
-                }
+                throw new Exception(result.Errors.First().Description);
+            }
 
-                result = userMgr.AddClaimsAsync(bob, new Claim[]{
-                            new Claim(JwtClaimTypes.Name, "Bob Smith"),
-                            new Claim(JwtClaimTypes.GivenName, "Bob"),
-                            new Claim(JwtClaimTypes.FamilyName, "Smith"),
-                            new Claim(JwtClaimTypes.WebSite, "http://bob.com"),
-                            new Claim("location", "somewhere")
-                        }).Result;
-                if (!result.Succeeded)
-                {
-                    throw new Exception(result.Errors.First().Description);
-                }
-                Log.Debug("bob created");
-            }
-            else
-            {
-                Log.Debug("bob already exists");
-            }
+            Log.Debug("alichavoshi created");
         }
+        else
+        {
+            Log.Debug("alichavoshi already exists");
+        }
+
+        // var bob = userMgr.FindByNameAsync("bob").Result;
+        // if (bob == null)
+        // {
+        //     bob = new ApplicationUser
+        //     {
+        //         UserName = "bob",
+        //         Email = "BobSmith@email.com",
+        //         EmailConfirmed = true
+        //     };
+        //     var result = userMgr.CreateAsync(bob, "Pass123$").Result;
+        //     if (!result.Succeeded)
+        //     {
+        //         throw new Exception(result.Errors.First().Description);
+        //     }
+        //
+        //     result = userMgr.AddClaimsAsync(bob, new Claim[]
+        //     {
+        //         new Claim(JwtClaimTypes.Name, "Bob Smith"),
+        //         new Claim(JwtClaimTypes.GivenName, "Bob"),
+        //         new Claim(JwtClaimTypes.FamilyName, "Smith"),
+        //         new Claim(JwtClaimTypes.WebSite, "http://bob.com"),
+        //         new Claim("location", "somewhere")
+        //     }).Result;
+        //     if (!result.Succeeded)
+        //     {
+        //         throw new Exception(result.Errors.First().Description);
+        //     }
+        //
+        //     Log.Debug("bob created");
+        // }
+        // else
+        // {
+        //     Log.Debug("bob already exists");
+        // }
     }
 }
