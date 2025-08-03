a23e75d61dbcf79eeac70597bb9ad95ea77801aa
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-08-03 15:12:40 +0330

    add some new component and routing to the project

--- Ecommerce/client/src/app/app.routes.ts
+++ Ecommerce/client/src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { StoreComponent } from './store/store.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'store',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.component').then((x) => x.HomeComponent)
+  },
+  {
+    path: 'contact-us',
+    loadComponent: () => import('./core/contact-us/contact-us.component').then((x) => x.ContactUsComponent)
+  },
+  {
+    path: 'store',
+    component: StoreComponent,
+    children: [
+      {
+        path: 'detail/:id',
+        loadComponent: () => import('./store/product-details/product-details.component').then((x) => x.ProductDetailsComponent)
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
