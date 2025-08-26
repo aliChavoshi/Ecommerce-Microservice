8a426e12283e61048dfa28a60d1923010354f000
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-08-26 16:13:53 +0330

    add basket style

--- Ecommerce/client/src/app/basket/home-basket/home-basket.component.ts
+++ Ecommerce/client/src/app/basket/home-basket/home-basket.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { BasketService } from '../basket.service';
+import { AsyncPipe, DecimalPipe } from '@angular/common';
+import { IProduct } from '../../shared/models/product';
 
 @Component({
   selector: 'app-home-basket',
-  imports: [],
+  imports: [AsyncPipe, DecimalPipe],
   templateUrl: './home-basket.component.html',
   styleUrl: './home-basket.component.css'
 })
 export class HomeBasketComponent {
-
+  constructor(public basketService: BasketService) {}
 }
