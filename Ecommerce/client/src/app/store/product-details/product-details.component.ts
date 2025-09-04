da553819fc762edc8d0b20f13d9e19daf76b54ef
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-09-04 15:14:01 +0330

    Enhance product details with basket integration
    Updated the product details component to conditionally render action buttons based on the product's presence in the basket. Replaced existing buttons with new ones that call `addItemToBasket()`.
    
    Introduced `BasketService` to manage basket state, added `ExistedInBasket` property, and implemented logic to check and update the basket status. The component now effectively tracks and manages product interactions related to the user's basket.

--- Ecommerce/client/src/app/store/product-details/product-details.component.ts
+++ Ecommerce/client/src/app/store/product-details/product-details.component.ts
@@ -4,23 +4,39 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { IProduct } from '../../shared/models/product';
 import { DecimalPipe } from '@angular/common';
 import { BreadcrumbService } from 'xng-breadcrumb';
+import { BasketService } from '../../basket/basket.service';
 
 @Component({
   selector: 'app-product-details',
-  imports: [RouterLink,DecimalPipe],
+  imports: [RouterLink, DecimalPipe],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent {
+  ExistedInBasket = false;
   private productId!: string;
-  product! : IProduct;
-  constructor(private storeService: StoreService, private route: ActivatedRoute,private bcService : BreadcrumbService) {
+  product!: IProduct;
+  constructor(private storeService: StoreService, private route: ActivatedRoute,
+     private bcService: BreadcrumbService, public basketService: BasketService) {
     this.productId = this.route.snapshot.params['id'];
   }
   ngOnInit(): void {
     this.getProductById();
+    this.existedInBasket();
   }
-
+  addItemToBasket(){
+    this.basketService.addItemToBasket(this.product).subscribe(res=>{
+      if(res){
+        this.ExistedInBasket = true;
+      }
+    });
+  }
+  private existedInBasket() {
+    const basket = this.basketService.getCurrentBasket();
+    if (basket?.items.some(x => x.productId == this.productId)) {
+      this.ExistedInBasket = true;
+    }
+  }
   private getProductById() {
     this.storeService.getProductById(this.productId).subscribe((res) => {
       this.product = res;
