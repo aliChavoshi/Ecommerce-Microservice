8f7613b779d1f086edcb0b282be23d297ed58299
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-09-08 17:12:58 +0330

    Refactor services and add new components with tests
    - Removed unused test setup in `account.service.spec.ts`.
    - Added `authInterceptor` in `auth.interceptor.ts`.
    - Created `SigninRedirectCallbackComponent` and its tests.
    - Created `SignoutRedirectCallbackComponent` and its tests.
    - Created `CheckoutComponent` and its tests.
    - Defined `CheckoutService` as injectable.

---
+++ Ecommerce/client/src/app/account/signin-redirect-callback/signin-redirect-callback.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signin-redirect-callback',
+  imports: [],
+  templateUrl: './signin-redirect-callback.component.html',
+  styleUrl: './signin-redirect-callback.component.css'
+})
+export class SigninRedirectCallbackComponent {
+
+}
---
+++ Ecommerce/client/src/app/account/signout-redirect-callback/signout-redirect-callback.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signout-redirect-callback',
+  imports: [],
+  templateUrl: './signout-redirect-callback.component.html',
+  styleUrl: './signout-redirect-callback.component.css'
+})
+export class SignoutRedirectCallbackComponent {
+
+}
