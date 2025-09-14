575c1a9aa21de7d28d5a0db14d10005fc9ccb53e
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-09-14 16:41:21 +0330

    refactor account service

--- Ecommerce/client/src/app/account/account.service.ts
+++ Ecommerce/client/src/app/account/account.service.ts
@@ -1,18 +1,39 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, ReplaySubject } from 'rxjs';
+import { User } from '../shared/models/user';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  private currentUserSource = new BehaviorSubject<unknown>(null);
+  private currentUserSource = new ReplaySubject<unknown>(1);
   currentUser$ = this.currentUserSource.asObservable();
-  constructor() {}
+  private user?: User | null;
+  token = '';
+  access_token = '';
 
-  finishLogin(): Promise<any> {
-    throw new Error('Method not implemented.');
+  constructor(private http: HttpClient, private router: Router) {}
+
+  isAuthenticated() {
+    return this.user != null && !this.user.expired;
   }
   login() {
+    throw new Error('Method not implemented.');
+  }
+  async signout() {}
+
+  public get authorizationHeaderValue(): string {
+    console.log('🚀 ~ AccountService ~ token :', this.token);
+    console.log('🚀 ~ AccountService ~ access_token:', this.access_token);
+
+    return `${this.token} ${this.access_token}`;
+  }
+  logout() {
+    
+  }
+  finishLogin(): Promise<any> {
     throw new Error('Method not implemented.');
   }
 }
